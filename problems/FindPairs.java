83e6afaeac12bd98702232d52e9116d3d563429b
AzharMd <47160438+AzharMd@users.noreply.github.com>
2019-03-18 01:25:44 +0530

    Update FindPairs.java

--- problems/FindPairs.java
+++ problems/FindPairs.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-John works at a clothing store. He has a large pile of socks that he must pair by color for sale. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+/*John works at a clothing store. He has a large pile of socks that he must pair by color for sale. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
 
 For example, there are  socks with colors . There is one pair of color  and one of color . There are three odd socks left, one of each color. The number of pairs is .
 
@@ -36,7 +36,8 @@ Sample Input
 10 20 20 10 10 30 50 10 20
 Sample Output
 
-3
+3*/
+
 public class Solution {
 
     // Complete the sockMerchant function below.
